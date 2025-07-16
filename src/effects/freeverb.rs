9cb324fa19b2b5ddac11bad824f51d5e954f6a62
Pablo Gaeta <pablofgaeta@gmail.com>
2025-07-16 12:47:38 -0500

    Add docs for freeverb parameters

--- src/effects/freeverb.rs
+++ src/effects/freeverb.rs
@@ -27,15 +27,67 @@ pub enum FreeverbMode {
     /// until unfrozen.
     Frozen,
 }
+mod tuning {
+    // Freeverb model tuning from: https://github.com/sinshu/freeverb/blob/main/Components/tuning.h
 
+    // Fixed gain to scale the input to the comb filters when not in freeze mode
+    // This also helps keep the output of the reverb from exceeding unity gain
+    pub const FIXED_GAIN: f32 = 0.03;
+
+    // 0.0 = small room, to 1.0 = large room; maps to 0.7 to 0.98
+    pub const SCALE_ROOM: f32 = 0.28;
+    pub const OFFSET_ROOM: f32 = 0.7;
+    pub const INITIAL_ROOM: f32 = 0.5;
+
+    // 0.0 = no damping, 1.0 = full damping; maps to 0.0 to 0.4
+    pub const INITIAL_DAMP: f32 = 0.5;
+    pub const SCALE_DAMP: f32 = 0.4;
+
+    pub const INITIAL_WET: f32 = 0.5; // 0.0 = no wet, 1.0 = full wet
+    pub const INITIAL_DRY: f32 = 0.0; // 0.0 = no dry, 1.0 = full dry
+    pub const INITIAL_WIDTH: f32 = 0.0; // 0.0 = mono, 1.0 = stereo
+
+    pub const ALLPASS_FEEDBACK: f32 = 0.5; // feedback coefficient for allpass filters
+
+    // Spread between left and right delay times for stereo effect
+    pub const STEREO_SPREAD_SEC: f32 = 0.000_521_541_9; // 23 samples (at 44.1kHz)
+
+    pub const NUM_COMBS: usize = 8;
+    pub const COMB_SECOND_TUNINGS: [f32; NUM_COMBS] = [
+        0.025_306_122, // 1116 samples at 44.1kHz
+        0.026_938_775, // 1188 samples at 44.1kHz
+        0.028_956_916, // 1277 samples at 44.1kHz
+        0.030_748_299, // 1356 samples at 44.1kHz
+        0.032_244_9,   // 1422 samples at 44.1kHz
+        0.033_809_524, // 1491 samples at 44.1kHz
+        0.035_306_122, // 1557 samples at 44.1kHz
+        0.036_666_666, // 1617 samples at 44.1kHz
+    ];
+
+    pub const NUM_ALLPASS: usize = 4;
+    pub const ALLPASS_SECOND_TUNINGS: [f32; NUM_ALLPASS] = [
+        0.012_607_709_7, // 556 samples at 44.1kHz
+        0.01,            // 441 samples at 44.1kHz
+        0.007_732_426_3, // 341 samples at 44.1kHz
+        0.005_102_040_8, // 225 samples at 44.1kHz
+    ];
+}
+/// Parameters to the freeverb effect.
 #[derive(Clone, Copy)]
-struct FreeverbParameters {
-    mode: FreeverbMode,
-    room_size: f32,
-    damp: f32,
-    wet: f32,
-    dry: f32,
-    width: f32,
+pub struct FreeverbParameters {
+    /// Mode for the reverb (active or frozen).
+    pub mode: FreeverbMode,
+    /// Size of the room to model reflections. 0.0 = small room to 1.0 = large room.
+    pub room_size: f32,
+    /// Amount of damping applied to high frequencies over time. 0.0 = no damping, 1.0 = full
+    /// damping.
+    pub damp: f32,
+    /// Mix of the reverb feedback signal to apply. 0.0 = no wet to 1.0 = full wet.
+    pub wet: f32,
+    /// Mix of the dry input signal to apply. 0.0 = no dry to 1.0 = full dry.
+    pub dry: f32,
+    /// Spatial spread of the reverb effect. 0.0 = mono to 1.0 = full stereo.
+    pub width: f32,
 }
 
 struct FreeverbDerivedVars {
@@ -121,28 +173,19 @@ impl<Storage: PCM, const N: usize> AudioNode<Stereo<f32>, Stereo<f32>> for Freev
 impl<S: PCM, const N: usize> Freeverb<S, N> {
     /// Construct a freeverb effect with the given initial parameters. All float parameters are
     /// checked to be within the range 0.0..=1.0.
-    ///
+    //
     /// ```
-    /// use dspkit::effects::{Freeverb, FreeverbMode};
-    /// let freeverb = Freeverb::<f32, 1024>::new(FreeverbMode::Active, 0.5, 0.5, 0.7, 0.3, 0.4);
+    /// use dspkit::effects::{Freeverb, FreeverbParameters, FreeverbMode};
+    /// let freeverb = Freeverb::<f32, 1024>::new(FreeverbParameters {
+    ///     mode: FreeverbMode::Active,
+    ///     room_size: 0.5,
+    ///     damp: 0.5,
+    ///     wet: 0.7,
+    ///     dry: 0.3,
+    ///     width: 0.4
+    /// });
     /// ```
-    pub fn new(
-        mode: FreeverbMode,
-        room_size: f32,
-        damp: f32,
-        wet: f32,
-        dry: f32,
-        width: f32,
-    ) -> Self {
-        let parameters = FreeverbParameters {
-            mode,
-            room_size,
-            damp,
-            wet,
-            dry,
-            width,
-        };
-
+    pub fn new(parameters: FreeverbParameters) -> Self {
         Self {
             parameters,
             derived: compute_derived_parameters(parameters),
@@ -264,50 +307,4 @@ const fn compute_derived_parameters(parameters: FreeverbParameters) -> FreeverbD
             damp: 0.0,
         },
     }
-}
-
-mod tuning {
-    // Freeverb model tuning from: https://github.com/sinshu/freeverb/blob/main/Components/tuning.h
-
-    // Fixed gain to scale the input to the comb filters when not in freeze mode
-    // This also helps keep the output of the reverb from exceeding unity gain
-    pub const FIXED_GAIN: f32 = 0.03;
-
-    // 0.0 = small room, to 1.0 = large room; maps to 0.7 to 0.98
-    pub const SCALE_ROOM: f32 = 0.28;
-    pub const OFFSET_ROOM: f32 = 0.7;
-    pub const INITIAL_ROOM: f32 = 0.5;
-
-    // 0.0 = no damping, 1.0 = full damping; maps to 0.0 to 0.4
-    pub const INITIAL_DAMP: f32 = 0.5;
-    pub const SCALE_DAMP: f32 = 0.4;
-
-    pub const INITIAL_WET: f32 = 0.5; // 0.0 = no wet, 1.0 = full wet
-    pub const INITIAL_DRY: f32 = 0.0; // 0.0 = no dry, 1.0 = full dry
-    pub const INITIAL_WIDTH: f32 = 0.0; // 0.0 = mono, 1.0 = stereo
-
-    pub const ALLPASS_FEEDBACK: f32 = 0.5; // feedback coefficient for allpass filters
-
-    // Spread between left and right delay times for stereo effect
-    pub const STEREO_SPREAD_SEC: f32 = 0.000_521_541_9; // 23 samples (at 44.1kHz)
-
-    pub const NUM_COMBS: usize = 8;
-    pub const COMB_SECOND_TUNINGS: [f32; NUM_COMBS] = [
-        0.025_306_122, // 1116 samples at 44.1kHz
-        0.026_938_775, // 1188 samples at 44.1kHz
-        0.028_956_916, // 1277 samples at 44.1kHz
-        0.030_748_299, // 1356 samples at 44.1kHz
-        0.032_244_9,   // 1422 samples at 44.1kHz
-        0.033_809_524, // 1491 samples at 44.1kHz
-        0.035_306_122, // 1557 samples at 44.1kHz
-        0.036_666_666, // 1617 samples at 44.1kHz
-    ];
-
-    pub const NUM_ALLPASS: usize = 4;
-    pub const ALLPASS_SECOND_TUNINGS: [f32; NUM_ALLPASS] = [
-        0.012_607_709_7, // 556 samples at 44.1kHz
-        0.01,            // 441 samples at 44.1kHz
-        0.007_732_426_3, // 341 samples at 44.1kHz
-        0.005_102_040_8, // 225 samples at 44.1kHz
-    ];
 }
